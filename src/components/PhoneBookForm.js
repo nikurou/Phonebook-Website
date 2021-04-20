62d50b7ad251b49fc033158e0d1e3fd1a0a03d91
nikurou <dylan.chung1997@gmail.com>
2021-04-20 00:22:36 -0700

    Formatting + test

--- src/components/PhoneBookForm.js
+++ src/components/PhoneBookForm.js
@@ -1,17 +1,24 @@
-import React from'react';
+import React from "react";
 
-const PhoneBookForm = ({name, number, handleNameChange, addPerson, handleNumberChange}) => {
-    return(
-        <form onSubmit = {addPerson}>
-          <div>
-            name: <input value = {name} onChange = {handleNameChange} /> <br/>
-            number: <input value = {number} onChange = {handleNumberChange}/>
-          </div>
-          <div>
-            <button type="submit" >add</button>
-          </div>
-        </form>
-    )
-  }
+//Component to handle input of new person
+const PhoneBookForm = ({
+  name,
+  number,
+  handleNameChange,
+  addPerson,
+  handleNumberChange,
+}) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={name} onChange={handleNameChange} /> <br />
+        number: <input value={number} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
 
-  export default PhoneBookForm
+export default PhoneBookForm;
