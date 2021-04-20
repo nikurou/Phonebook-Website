77045113872178abcb7b19341eea9e2020e28f6f
nikurou <dylan.chung1997@gmail.com>
2021-04-20 00:20:02 -0700

    Formatting Update

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,21 +1,25 @@
-import React from'react';
+import React from "react";
 
 //Display of the phonebook contents
-const Numbers = ({persons, filter, handleDeleteUser}) =>{
 
-    const filteredArray = persons.filter(element => (element.name.toLowerCase()).includes(filter.toLowerCase()) )
-    
-    return(
-      <div>
-        {filteredArray.map(person => 
-          <div key = {person.name}>
-            <span> {person.name} {person.number} </span> 
-            <button  onClick = {()=>handleDeleteUser(person)}> Delete </button>
-            <br/>
-          </div>
-          )}
-      </div>
-    )
-  }
-  
+const Numbers = ({ persons, filter, handleDeleteUser }) => {
+  const filteredArray = persons.filter((element) =>
+    element.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      {filteredArray.map((person) => (
+        <div key={person.name}>
+          <span>
+            {person.name} {person.number}
+          </span>
+          <button onClick={() => handleDeleteUser(person)}> Delete </button>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default Numbers;
